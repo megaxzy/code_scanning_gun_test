cae9e6101c8fd2ae91c6135ec8063ef8c7e3bb26
919482827 <919482827@qq.com>
2018-06-27 00:43:32 +0800

    add one button

---
+++ test2/MainForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace test2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.TextBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.Location = new System.Drawing.Point(12, 39);
+            this.TextBox1.MaxLength = 100;
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(177, 21);
+            this.TextBox1.TabIndex = 0;
+            this.TextBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            this.TextBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox1_KeyPress_1);
+            // 
+            // TextBox2
+            // 
+            this.TextBox2.Location = new System.Drawing.Point(233, 39);
+            this.TextBox2.Multiline = true;
+            this.TextBox2.Name = "TextBox2";
+            this.TextBox2.Size = new System.Drawing.Size(100, 277);
+            this.TextBox2.TabIndex = 1;
+            this.TextBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TextBox2);
+            this.Controls.Add(this.TextBox1);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBox1;
+        private System.Windows.Forms.TextBox TextBox2;
+    }
+}
+
---
+++ test2/MainForm.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test2
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private string textbox1;
+        private string textbox2;
+
+        private static bool time_first = true;
+
+        private DateTime dt = DateTime.Now;  //定义一个成员函数用于保存每次的时间点
+        public void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+            TextBox1.MaxLength = 100;
+            DateTime tempDt = DateTime.Now;          //保存按键按下时刻的时间点
+            TimeSpan ts = tempDt.Subtract(dt);     //获取时间间隔
+
+            string text_temp = TextBox1.Text.ToString();
+            long target = 0;                           //这里如果用int会不够长
+            if (long.TryParse(text_temp, out target) == true) //是不是整形
+            {
+                if (ts.Milliseconds > 25 && time_first == false)  //判断时间间隔，如果时间间隔大于???毫秒，则将TextBox清空
+                {
+                    TextBox1.Text = "";
+                }
+                else
+                {
+                    time_first = false;
+                    dt = tempDt;
+                    textbox1 = TextBox1.Text;
+                    textbox2 += textbox1[textbox1.Length - 1];
+                    textbox2 += "  ";
+                    textbox2 += ts.Milliseconds;
+                    textbox2 += "\r\n";
+                }
+            }
+            else
+            {
+                TextBox1.Text = "";
+            }
+        }
+
+
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+            if (textbox2.Length >= 16)//到时候改 因为测试阶段有时间差等等内容
+            {
+                TextBox2.Text = textbox2;
+
+            }
+            else
+                TextBox2.Text = "";
+            //TextBox2.Text=textbox2;
+        }
+
+        private void TextBox2_ParentChanged(object sender, EventArgs e)
+        {
+        }
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(Char.IsNumber(e.KeyChar)) && e.KeyChar != (char)13 && e.KeyChar != (char)8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void TextBox1_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 'a')
+            {
+                MessageBox.Show("Wrong Input");
+            }
+        }
+    }
+}
